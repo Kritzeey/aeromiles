00733febd981cbd507f90bdcf16934b62c04590e
valhize <190005533+Kritzeey@users.noreply.github.com>
2026-05-07 16:28:51 +0700

    feat: add authentication app

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, "main/index.html")
+def home_view(request):
+    return render(request, "main/home.html")
